320a51b2d239c2fddeb8957b59b21f60db49fb07
jherreral <33136201+jherreral@users.noreply.github.com>
2023-04-24 18:38:18 -0400

    Achieved download of Excel for account movements.

--- boc_retrieval.py
+++ boc_retrieval.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,8 +11,16 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+#To run this in console
+# exec(open('boc_retrieval.py').read())
+
+#Set options
+firefox_options = FirefoxOptions()
+firefox_options.set_preference("browser.download.folderList",2)
+firefox_options.set_preference("browser.download.dir","D:\\Repos\\boc_retrieval\\")
+
 # Install geckodriver
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()),options=firefox_options)
 
 # Get credentials
 with open("credentials.json", "r") as infile:
@@ -31,31 +40,43 @@ password_field.send_keys(my_password)
 password_field.send_keys(Keys.RETURN)
 
 # Wait for page to load
-# wait = WebDriverWait(driver, 10)
-# wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+wait = WebDriverWait(driver, 10)
+wait.until(EC.presence_of_element_located((By.ID, "btn-home_CuentaCorrienteMonedaLocal")))
 # driver.implicitly_wait(8)
 
+# Go to account page
+try:
+    modal_close_button = driver.find_element(By.XPATH,"//button[@class='btn default btn pull-right']")
+    modal_close_button.click()
+except NoSuchElementException:
+    print("No modal found")
 
 # Go to account page
-# try:
-#     modal_close_button = driver.find_element(By.XPATH,"//button[@id='modal_emergente_close']")
-#     modal_close_button.click()
-# except NoSuchElementException:
-#     print("No modal found")
+try:
+    account_button = driver.find_element(By.ID,"btn-home_CuentaCorrienteMonedaLocal")
+    driver.execute_script("arguments[0].click()",account_button)
+except NoSuchElementException:
+    print("No account button found")
 
-# account_button = driver.find_element(By.ID,"btn-home_CuentaCorrienteMonedaLocal")
-# #account_button = driver.find_element(By.LINK_TEXT,"Cuenta Corriente")
-# driver.execute_script("arguments[0].scrollIntoView();", account_button)
-# account_button.click()
+# Wait for page to load
+wait = WebDriverWait(driver, 10)
+wait.until(EC.presence_of_element_located((By.XPATH, "//bch-button[@text='Descargar']")))
 
 # Click download button
-# download_button = driver.find_element_by_xpath("//button[@id='download']")
-# download_button.click()
+try:
+    download_button = driver.find_element(By.XPATH,"//bch-button[@text='Descargar']") 
+    download_button.click()
+except NoSuchElementException:
+    print("No download button found")
 
-# # Save file
-# url = driver.current_url
-# response = requests.get(url)
-# soup = BeautifulSoup(response.content, 'html.parser')
-# filename = soup.find('filename').text
-# with open(filename, 'wb') as f:
-#     f.write(response.content)
+# Wait for page to load
+wait = WebDriverWait(driver, 10)
+wait.until(EC.presence_of_element_located((By.XPATH, "//button[@role='menuitem']")))
+
+# Click "Descargar Excel" and save file
+dl_buttons = driver.find_elements(By.XPATH,"//button[@role='menuitem']")
+if dl_buttons:
+    driver.execute_script("arguments[0].click()",dl_buttons[0])
+else:
+    print("No Excel download button found")
+
